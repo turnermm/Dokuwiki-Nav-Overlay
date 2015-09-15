9f17ebcaad0f752c5e3f34050798ca1ecbd52b5d
Myron Turner <turnermm02@shaw.ca>
2015-09-14 21:46:33 -0500

    added insertion of overlay div using TPL_ACT_RENDER and toggle tool using TEMPLATE_PAGETOOLS_DISPLAY

--- action.php
+++ action.php
@@ -10,12 +10,12 @@
     
         function register(Doku_Event_Handler $controller) {             
             $controller->register_hook('DOKUWIKI_STARTED', 'AFTER',  $this, 'set_admin');
+            $controller->register_hook('TPL_ACT_RENDER', 'AFTER',  $this, 'print_overlay', array('after'));
+           $controller->register_hook('TEMPLATE_PAGETOOLS_DISPLAY', 'BEFORE', $this, 'action_link');    
         }
         
         function set_admin(&$event, $param) {
             global $JSINFO, $ACT;         
-            
-           
             
             if($this->getConf('always')) {
                 $JSINFO['overlay'] = 1;
@@ -31,6 +31,23 @@
               $regex = trim($regex, '|');            
              $JSINFO['overlay'] = preg_match("/" . $regex ."/",$ACT);      
            }
-        }        
+        }
+
+        function print_overlay(&$event, $param) {        
+         $insert =  p_wiki_xhtml('wiki:nav');  
+$text = <<<TEXT
+       <div id='overlay'><div  class = "close">
+        <a href="javascript:jQuery('#overlay').toggle();void(0);"  rel="nofollow" title="close">Close</a>
+        </div> $insert</div>
+TEXT;
+          echo $text;
+      //   echo '<li><a href="javascript:jQuery(\'#overlay\').toggle();void(0);"  rel="nofollow"   title="Index">Index</a></li>';
+        }
+        
+   function action_link(&$event, $param)
+    {
+        $event->data['items']['overlay'] = '<li><a href="javascript:jQuery(\'#overlay\').toggle();void(0);"  rel="nofollow"   title="Index">Index</a></li>';
+
+    }        
     }
 
