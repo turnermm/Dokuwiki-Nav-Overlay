6f00b2d2a36b92475c247a84fa551876662119fe
Myron Turner <turnermm02@shaw.ca>
2015-09-19 19:21:35 -0500

    implemented  width and height in script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ if(JSINFO && !JSINFO['overlay']) {
 
 jQuery( document ).ready(function() {   
     jQuery( "#overlay" ).draggable();
-   /*  jQuery( "#overlay" ).css('width','400px'); */
+    if(JSINFO ) {
+       if(JSINFO['ol_width']) {
+           jQuery( "#overlay" ).css('width',JSINFO['ol_width']); 
+       }    
+       if(JSINFO['ol_height']) {
+           jQuery( "#overlay" ).css('height',JSINFO['ol_height']);            
+        }
+   }
+  
 });
 
    
