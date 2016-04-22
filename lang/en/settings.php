584642e919a9f64ca9984d6c8e138b047724e7bb
Myeongjin <aranet100@gmail.com>
2016-04-22 15:14:02 +0900

    remove unnecessary spaces

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -12,10 +12,10 @@ $lang['edit'] = 'Initially hide overlay during edits.';
 $lang['always'] = 'Always initially hide the overlay.';
 $lang['page'] = 'Dokuwiki page on which navigation or overlay content is to be found [namespace:pagename].';
 $lang['menutype'] ="Where to place overlay toggle (see overlay plugin page).";
-$lang['nsoverlays'] = "Comma separated list of namespaces where alternate overlay content will be found for display in those namespaces. The pages with this alternate content "
-  . " must be named 'overlay' and if found  will be used instead of the  main overlay page. If a namespace ends with an asterisk, it will govern child namespaces, unless"
+$lang['nsoverlays'] = "Comma separated list of namespaces where alternate overlay content will be found for display in those namespaces. The pages with this alternate content"
+  . " must be named 'overlay' and if found will be used instead of the main overlay page. If a namespace ends with an asterisk, it will govern child namespaces, unless"
   . " a child has its own overlay page.";
-$lang['width'] ="Width in pixels of the overlay window.  This can also be changed in overlay/style.css. Its initial setting is 225px.";
-$lang['height'] ="Height in pixels of the overlay window. This  can also be changed in overlay/style.css. Its initial setting is 325px.";
+$lang['width'] ="Width in pixels of the overlay window. This can also be changed in overlay/style.css. Its initial setting is 225px.";
+$lang['height'] ="Height in pixels of the overlay window. This can also be changed in overlay/style.css. Its initial setting is 325px.";
 $lang['tools'] = 'Which action links to insert in top menu bar of overlay panel. A comma separated list of unspecified links can be inserted in the textbox. The width of the panel may have to be increased in size for some sets of link names.';
 $lang['toggletype'] = "If you are not using the dokuwiki template or a compatible template, and you are implementing the tools event (see the plugin page at dokuwiki.org), select whether you want to insert a button or a link.";
