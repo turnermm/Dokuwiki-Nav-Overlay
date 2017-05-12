a3737e102a33d9792ddfd530310add0cac1c5809
Myron Turner <turnermm02@shaw.ca>
2017-05-12 16:22:18 -0500

    js for saving overlay position to cookie

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ if(JSINFO && !JSINFO['overlay']) {
 }
 
 jQuery( document ).ready(function() {   
-    jQuery( "#overlay" ).draggable();
+  var theUserposition= {'x':0,'y':0};        
+    jQuery( "#overlay" ).draggable({
+        drag: function(event,ui){
+             var offset = jQuery(this).offset();
+            var xPos = offset.left;
+            var yPos = offset.top;
+        }, 
+        stop: function(){
+            var finalOffset = jQuery(this).offset();
+            theUserposition.x = finalOffset.left;
+            theUserposition.y = finalOffset.top;          
+        },
+    });
     if(JSINFO ) {
        if(JSINFO['ol_width']) {
            jQuery( "#overlay" ).css('width',JSINFO['ol_width']); 
@@ -14,7 +26,16 @@ jQuery( document ).ready(function() {
            jQuery( "#overlay" ).css('height',JSINFO['ol_height']);            
         }
    }
-  
+ 
+ jQuery(window).on('beforeunload', function(){ 
+    var pos = (theUserposition.x).toString() + '#' + (theUserposition.y).toString();
+    setOverlayCookie('OverlayUserposition', pos) ;
+});
 });
 
-   
+function setOverlayCookie(cname, cvalue) {
+    var d = new Date();  
+    d.setTime(d.getTime() + (10*60*1000)); //10 minutes
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";    
+}
