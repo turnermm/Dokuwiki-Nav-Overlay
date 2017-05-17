3bfe439889980ad4ad408455cece1721d284403d
Myron Turner <turnermm02@shaw.ca>
2017-05-16 21:44:38 -0500

    updated settings text for width to 300 from 225

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -15,7 +15,7 @@ $lang['menutype'] ="Where to place overlay toggle (see overlay plugin page).";
 $lang['nsoverlays'] = "Comma separated list of namespaces where alternate overlay content will be found for display in those namespaces. The pages with this alternate content"
   . " must be named 'overlay' and if found will be used instead of the main overlay page. If a namespace ends with an asterisk, it will govern child namespaces, unless"
   . " a child has its own overlay page.";
-$lang['width'] ="Width in pixels of the overlay window. This can also be changed in overlay/style.css. Its initial setting is 225px.";
+$lang['width'] ="Width in pixels of the overlay window. This can also be changed in overlay/style.css. Its initial setting is 300px.";
 $lang['height'] ="Height in pixels of the overlay window. This can also be changed in overlay/style.css. Its initial setting is 325px.";
 $lang['tools'] = 'Which action links to insert in top menu bar of overlay panel. A comma separated list of unspecified links can be inserted in the textbox. The width of the panel may have to be increased in size for some sets of link names.';
 $lang['toggletype'] = "If you are not using the dokuwiki template or a compatible template, and you are implementing the tools event (see the plugin page at dokuwiki.org), select whether you want to insert a button or a link.";
