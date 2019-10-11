211c2ebaa6ce01db6739e1ea68f0d1475678c3de
Aleksandr Selivanov <alexgearbox@yandex.ru>
2019-10-11 23:17:59 +0400

    Russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['toggle_name'] = 'Накладка';
+$lang['close'] = 'Закрыть';
+$lang['fix_title'] = 'Включить перемещение вместе со страницей';
+$lang['js']['fix_title'] = 'Включить перемещение вместе со страницей';
+$lang['js']['unfix_title'] = 'Зафиксировать положение на экране';
+$lang['detach'] = 'Закрепить';
+$lang['attach'] = 'Открепить';
+$lang['js']['detach'] = 'Закрепить';
+$lang['js']['attach'] = 'Открепить';
+$lang['js']['absolute'] = 'Закрепить';
+$lang['js']['fixed'] = 'Открепить';
+$lang['resize'] = 'Изменить размер';
